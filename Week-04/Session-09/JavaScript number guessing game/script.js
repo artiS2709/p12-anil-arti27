0eb28723db5a5e49bfe95a1228396199068a3761
Arti singh <artisingh2724@gmail.com>
2023-07-29 06:56:34 -0700

    update assignment

--- Week-04/Session-09/JavaScript number guessing game/script.js
+++ Week-04/Session-09/JavaScript number guessing game/script.js
@@ -1,9 +1,35 @@
 // JavaScript number guessing game
-let totalGuesses=0;
-let numToGuess = prompt("Enter a number between 1 and 100");
+
+// Counting the number of guesses
+// made for correct Guess
+let guess=1;
+
 function playGuessingGame(numToGuess,totalGuesses){
-    totalGuesses++;
-    alert(totalGuesses);
+    
+    let userVal = prompt("Enter a number between 1 and 100");
+    // Generate a Random Number
+    //let randomNum = Math.floor(Math.random() * 10 + 1);
+    let randomNum =3;
+    //console.log(randomNum);
+    //console.log(numToGuess);
+    while(totalGuesses){
+    if (numToGuess == randomNum) {
+        return guess;
+    }else if (numToGuess > randomNum && numToGuess!="") {
+        guess++;
+        prompt(numToGuess+" is too large. Guess a smaller number.");
+    }else if(numToGuess < randomNum && numToGuess!=""){
+        guess++;
+        prompt(numToGuess+" is too small. Guess a larger number");
+    }else if(isNaN(numToGuess) || numToGuess==""){
+        prompt("Please enter a number");
+    }else{
+        return 0;
+    }
 }
 
-playGuessingGame();
+}
+
+playGuessingGame(numToGuess,totalGuesses);
+
+ 
