66b174652222bd188c7ca63942d2ce3caeb164aa
Arti singh <artisingh2724@gmail.com>
2023-07-29 08:13:38 -0700

    JS Basics

--- Week-04/Session-09/JavaScript number guessing game/script.js
+++ Week-04/Session-09/JavaScript number guessing game/script.js
@@ -1,35 +1,62 @@
-// JavaScript number guessing game
+// Your solution goes here 
+let playGuessingGame = (numToGuess, totalGuesses) => {
+    try {
+        let guessedNumber;
+        totalGuesses = totalGuesses || 10;
+        let guessCount = 0;
 
-// Counting the number of guesses
-// made for correct Guess
-let guess=1;
+        for (let a = 0; a < totalGuesses; a++) {
 
-function playGuessingGame(numToGuess,totalGuesses){
-    
-    let userVal = prompt("Enter a number between 1 and 100");
-    // Generate a Random Number
-    //let randomNum = Math.floor(Math.random() * 10 + 1);
-    let randomNum =3;
-    //console.log(randomNum);
-    //console.log(numToGuess);
-    while(totalGuesses){
-    if (numToGuess == randomNum) {
-        return guess;
-    }else if (numToGuess > randomNum && numToGuess!="") {
-        guess++;
-        prompt(numToGuess+" is too large. Guess a smaller number.");
-    }else if(numToGuess < randomNum && numToGuess!=""){
-        guess++;
-        prompt(numToGuess+" is too small. Guess a larger number");
-    }else if(isNaN(numToGuess) || numToGuess==""){
-        prompt("Please enter a number");
-    }else{
-        return 0;
+            //first-prompt section
+            if (guessCount == 0) {
+                guessedNumber = prompt("Enter a number between 1 and 100.");
+
+                if (isNaN(guessedNumber) || guessedNumber == "") {
+                    guessedNumber = prompt("Please enter a number.");
+
+                }
+                guessCount++
+
+            } else {
+
+                //perfect guess section
+                if (guessedNumber == numToGuess) {
+                    return guessCount
+                }
+
+                //smaller guess section
+                if (guessedNumber && guessedNumber < numToGuess) {
+                    guessedNumber = prompt(`${guessedNumber}  is too small. Guess a larger number.`);
+
+                    if (isNaN(guessedNumber) || guessedNumber == "") {
+                        guessedNumber = prompt("Please enter a number.");
+                    } else {
+                        guessCount++
+                    }
+
+                }
+
+                //greater guess section
+                if (guessedNumber && guessedNumber > numToGuess) {
+                    guessedNumber = prompt(`${guessedNumber} is too large. Guess a smaller number.`);
+
+                    if (isNaN(guessedNumber) || guessedNumber == "") {
+                        guessedNumber = prompt("Please enter a number.");
+                    } else {
+                        guessCount++
+                    }
+
+                }
+
+            }
+        }
+
+        //false input or guess exceed section
+        if (!guessedNumber || (guessCount >= (totalGuesses - 1))) {
+            return 0
+        }
+    } catch (error) {
+        return { messagee: error }
     }
-}
 
 }
-
-playGuessingGame(numToGuess,totalGuesses);
-
- 
